f7e2c7a0fc42880f71d31b10b558e8cfdae254a5
Kejkey515 <Kejkey515@users.noreply.github.com>
2026-07-10 16:04:55 +0600

    Fix entrypoint - use Python heredoc for reliable password injection

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,14 @@ set -e
 
 # Inject password from environment variable if set
 if [ -n "$BEEF_PASSWORD" ]; then
-    sed -i "s/CHANGE_ME_BEEF_PASSWORD/$BEEF_PASSWORD/g" /beef/config.yaml
+    python3 << 'PYEOF'
+import os
+with open('/beef/config.yaml', 'r') as f:
+    content = f.read()
+content = content.replace('CHANGE_ME_BEEF_PASSWORD', os.environ['BEEF_PASSWORD'])
+with open('/beef/config.yaml', 'w') as f:
+    f.write(content)
+PYEOF
 fi
 
 # Start BeEF
